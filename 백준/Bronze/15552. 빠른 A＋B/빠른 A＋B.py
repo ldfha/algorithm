4372e9085792b76868c47111ab7ad424ec269725
ldfha <148486425+ldfha@users.noreply.github.com>
2026-02-04 13:20:19 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 852 ms, Memory: 94828 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -1,9 +1,8 @@
 import sys
+input = sys.stdin.readline
 n = int(input())
 str = []
-for i in range(n):
-    str.append(sys.stdin.readline().rstrip())
-# print('str',str)
-for s in str:
-    a, b = map(int, s.split())
-    print(a+ b)
+for _ in range(n):
+    a, b = map(int, input().split())
+    str.append(a+b)
+print(*str, sep='\n')
