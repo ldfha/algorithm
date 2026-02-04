d46b9000a77c7f9970165b7a957ee8ad7b12c13e
ldfha <148486425+ldfha@users.noreply.github.com>
2026-02-04 11:23:34 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,9 @@
+a = input()
+b = input()
+b_list = list(b)
+third = int(a)*int(b[2])
+forth = int(a)*int(b[1])
+fifth = int(a)*int(b[0])
+final = int(a)*int(b)
+
+print(f'{third}\n{forth}\n{fifth}\n{final}')
