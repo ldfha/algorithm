ded18c39a29421fe573385dca9b3abe6a52980de
ldfha <148486425+ldfha@users.noreply.github.com>
2026-02-11 13:13:25 +0900

    [Bronze II] Title: 수 정렬하기, Time: 64 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -2,9 +2,6 @@ N = int(input())
 numbers = []
 for _ in range(N):
     numbers.append(int(input()))
-for i in range(N):
-    for j in range(i+1, N):
-        if numbers[i] > numbers[j]:
-            numbers[i], numbers[j] = numbers[j], numbers[i]
+numbers.sort()
 for n in numbers:
     print(n)
