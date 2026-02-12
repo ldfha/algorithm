d46c8e138bbb035b4405047b6efe539b3a55d7b8
ldfha <148486425+ldfha@users.noreply.github.com>
2026-02-12 14:25:39 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2780 ms, Memory: 52648 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,14 @@
+N = int(input())
+points = []
+for i in range(N):
+    points.append(list(map(int, input().split())))
+# for i in range(N):  # 0 1 2 3 4
+#     for j in range(i + 1, N):
+#         if points[i][0] > points[j][0]:
+#             points[i], points[j] = points[j], points[i]
+#         elif points[i][0] == points[j][0]:
+#             if points[i][1] > points[j][1]:
+#                 points[i], points[j] = points[j], points[i]
+points.sort()
+for p in points:
+    print(*p) 
