2c7a3530927cadaa685e5745995192fa65fbf9d7
Suyeon Hwang <148486425+ldfha@users.noreply.github.com>
2026-03-24 23:44:55 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.01 ms, Memory: 4.46 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n)
+{
+    int answer = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        answer++;
+        if (answer % 3 == 0 || (answer % 10) == 3 || (answer / 10) == 3)
+        {
+            answer++;
+            while (answer % 3 == 0 || (answer % 10) == 3 || (answer / 10) == 3 || (answer % 100) / 10 == 3)
+            {
+                answer++;
+            }
+        }
+    }
+    return answer;
+}
