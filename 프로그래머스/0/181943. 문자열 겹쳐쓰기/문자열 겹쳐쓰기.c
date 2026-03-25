2131411991bad780014b9a9d0f7c21a9da603d00
Suyeon Hwang <148486425+ldfha@users.noreply.github.com>
2026-03-25 09:57:38 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.c
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.c
@@ -6,9 +6,9 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* my_string, const char* overwrite_string, int s) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    char* answer = (char*)malloc(sizeof(char)*(strlen(my_string)+1));
-    strncpy(answer, my_string, s);
-    strcpy(answer+s, overwrite_string);
-    strcpy(answer+s+strlen(overwrite_string), my_string+s+strlen(overwrite_string));
+    int len = strlen(my_string);
+    char* answer = (char*)malloc(len + 1);
+    strcpy(answer, my_string);
+    memcpy(answer+s, overwrite_string, sizeof(char) * strlen(overwrite_string));
     return answer;
 }
