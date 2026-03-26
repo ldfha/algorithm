a110763a85757394450179c199bc99ea144a9910
Suyeon Hwang <148486425+ldfha@users.noreply.github.com>
2026-03-26 23:27:46 +0900

    [level 2] Title: 올바른 괄호, Time: 0.08 ms, Memory: 3.96 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+bool solution(const char* s) {
+    bool answer = true;
+    if (s[0] != '(') { return false; }
+    int count = 0;
+    int i = 0;
+    while(i < strlen(s)){
+        if(s[i] == '('){
+            count ++;
+        } else{
+            count --;
+        }
+        if(count < 0) return false;
+        i++;
+    }
+    answer = (count == 0) ? true : false;
+    return answer;
+}
