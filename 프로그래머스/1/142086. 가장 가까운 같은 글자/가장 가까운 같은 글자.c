bb33f6c4efe074704d8ee550d08b436f7f493c73
Suyeon Hwang <148486425+ldfha@users.noreply.github.com>
2026-03-26 16:51:31 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 46.13 ms, Memory: 5.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int* solution(const char* s) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)calloc(strlen(s), sizeof(int));
+    answer[0]=-1;
+    for(int i=1; i<strlen(s); i++){
+        char c = s[i];
+        for(int j=0; j<i; j++){
+            if(s[j] == c){
+                if(answer[i]==0){
+                    answer[i]=i-j;
+                } else {
+                    answer[i] = (answer[i] > i - j)? i-j : answer[i];
+                }
+            }
+        }
+        if(answer[i] == 0) {answer[i] = -1;}
+    }
+    return answer;
+}
