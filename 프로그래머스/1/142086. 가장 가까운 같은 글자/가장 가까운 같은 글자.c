edc2f7dad91285409f76f56261f51a5a6895177d
Suyeon Hwang <148486425+ldfha@users.noreply.github.com>
2026-03-26 17:10:30 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.44 ms, Memory: 5.71 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.c
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.c
@@ -5,20 +5,30 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int* solution(const char* s) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int* answer = (int*)calloc(strlen(s), sizeof(int));
-    answer[0]=-1;
-    for(int i=1; i<strlen(s); i++){
-        char c = s[i];
-        for(int j=0; j<i; j++){
-            if(s[j] == c){
-                if(answer[i]==0){
-                    answer[i]=i-j;
-                } else {
-                    answer[i] = (answer[i] > i - j)? i-j : answer[i];
-                }
-            }
-        }
-        if(answer[i] == 0) {answer[i] = -1;}
+    int* answer = (int*)malloc(strlen(s) * sizeof(int));
+    int len = strlen(s);
+    int check[26];
+    for (int i = 0; i < 26; i++) {
+        check[i] = -1;
     }
+    for(int i = 0; i < len; i++){
+        unsigned char c = (unsigned char)s[i] - 97;
+        answer[i] = (check[c] == -1) ? -1 : i - check[c];
+        check[c] = i;
+    }
+    // answer[0]=-1;
+    // for(int i=1; i<strlen(s); i++){
+    //     char c = s[i];
+    //     for(int j=0; j<i; j++){
+    //         if(s[j] == c){
+    //             if(answer[i]==0){
+    //                 answer[i]=i-j;
+    //             } else {
+    //                 answer[i] = (answer[i] > i - j)? i-j : answer[i];
+    //             }
+    //         }
+    //     }
+    //     if(answer[i] == 0) {answer[i] = -1;}
+    // }
     return answer;
 }
